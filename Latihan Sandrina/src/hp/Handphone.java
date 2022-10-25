5907f4a43e060dbf3ae35e07ae1331b282e331b6
Sandrina16 <115547048+Sandrina16@users.noreply.github.com>
2022-10-25 18:18:00 +0700

    Add files via upload

---
+++ Latihan Sandrina/src/hp/Handphone.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hp;
+
+/**
+ *
+ * @author hp
+ */
+public class Handphone {
+        public void hidupkan(){
+             System.out.println("Handphone Hidup...");
+        }
+              public void lakukanPanggilan(){
+                   System.out.println("Kring,Kring...panggilan dilakukan ");
+        }
+              public void kirimSMS (){
+                  System.out.println("Dung,dung...sms berhasil dikirim");
+    }
+     public void matikan (){
+          System.out.println("Handphome Mati...");
+     }
+}
